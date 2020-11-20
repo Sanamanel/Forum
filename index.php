e46ba0559785fffc7d18817aec0fd15c17a3389c
User <rachida.haddouch.webdev@gmail.com>
2020-11-20 13:39:00 +0100

    new index

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-<?php
-echo 'bonjour monde';
-?>
-</body>
-</html>
+<?php include("header.php"); ?>
+
+<?php include("main-home.php"); ?>
+<?php include("aside.php"); ?>
+
+<?php include("footer.php"); ?>
