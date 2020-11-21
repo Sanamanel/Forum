9d65a51a2efd9ade95b253142395b2c8b56d57df
User <rachida.haddouch.webdev@gmail.com>
2020-11-21 19:46:09 +0100

    update index

--- led-conf/logout.php
+++ led-conf/logout.php
@@ -3,7 +3,7 @@ session_start();
 session_unset();
 session_destroy();
 unset($_SESSION["var"]); // On efface la variable var
-header("location: login.php" ) ; // On renvoie ensuite sur la page d'accueil
+header("location: index.php" ) ; // On renvoie ensuite sur la page d'accueil
 exit();
 ?>
 
