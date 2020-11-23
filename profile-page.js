dc6c6dd817ea52350d18d2ecd0057e07d3cb6d6b
iCarolinei <iCarolinei@github.com>
2020-11-23 20:17:46 +0100

    tryPhpProfile

---
+++ profile-page.js
@@ -0,0 +1,46 @@
+(async function() {
+    
+    let baseUrl = "http://localhost/forum_bfr/"; //to be adapted
+
+        $.ajax({
+            url: baseUrl + 'server.php',
+            type: 'get',
+            data: {action : 'profile' },
+            complete: function(response) {
+                console.log(response);
+                
+                if(response.status == 200) //The server have send information
+                {
+                    const profile = JSON.parse(response.responseText); //Decode JSON sent by the server
+                    document.getElementById("email").value = profile.email;
+                    document.getElementById("firstname").value = profile.firstname;
+                    document.getElementById("lastname").value = profile.lastname;
+                    document.getElementById("username").value = profile.username;
+                    
+                    //document.getElementById("birthdate").value = profile.birthdate.format('MM/DD/YYYY');
+                    //document.getElementById("country").value = profile.country;
+                    document.getElementById("signature").value = profile.signature;
+                    document.getElementById("username_display").innerHTML = profile.username;
+                    document.getElementById("signature_display").innerHTML = profile.signature;
+                    document.getElementById("fullname_display").innerHTML = `${Strings.orEmpty(profile.firstname)} ${Strings.orEmpty(profile.lastname) }`;
+
+                }
+                else if (response.status == 401 ) // When user is not authenticated, redirect to login page
+                    window.location.href = baseUrl + 'index.php';
+                else if (response.status == 500 ) //Internal error 
+                    window.location.href = baseUrl + 'index.php';
+                else 
+                    window.location.href = baseUrl + 'index.php';
+
+            }
+        });
+
+        document.getElementById("btn_save").addEventListener("click", async () => {
+
+        })
+
+        const Strings = {};
+        Strings.orEmpty = function( entity ) {
+        return entity || "";
+};
+})();
