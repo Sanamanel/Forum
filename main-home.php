fe05129f82314376d57f658feeea3d544787e26e
Caroline <71645461+iCarolinei@users.noreply.github.com>
2020-11-25 18:32:38 +0100

    fix

--- main-home.php
+++ main-home.php
@@ -23,10 +23,9 @@
 
             //---Start Board
             $sql = 'select * from board ';  // Fetch All Rows From Board Table
-            var_dump("---");
-            var_dump($conn);
-            $result = $conn->exec($sql);    // Connect to Database and Query from Database
-            var_dump($result);
+   
+            $result = $conn->query($sql);    // Connect to Database and Query from Database
+        
             $cats = array();                  //This Array Stor all Categories
             $cats_id = array();                  //This Array Stor all Categories
             $counter=0;
