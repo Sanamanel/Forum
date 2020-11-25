e9e5fbda147b01ca925361e2c352e0e9d72eb552
AliMundher <mundhersaeedali@gmail.com>
2020-11-25 16:30:12 +0100

    Add pdo to file connect

--- connect.php
+++ connect.php
@@ -4,10 +4,12 @@
             $password = 'vkN1eNSWhe';
 
             //On établit la connexion
-            $conn = new mysqli($servername, $username, $password);
-
-            //On vérifie la connexion
-            if($conn->connect_error){
-                die('Erreur : ' .$conn->connect_error);
+            try{
+                $conn = new PDO($servername, $username, $password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
             }
+            catch(PDOEXCEPTION $e){
+                echo "Erro";
+            }
+            
 ?>
