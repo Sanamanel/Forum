fb7e685fb01db500b8a5620ced869b279d263c8b
User <rachida.haddouch.webdev@gmail.com>
2020-11-27 15:05:36 +0100

    update button

--- main-home.php
+++ main-home.php
@@ -64,7 +64,7 @@
               <?php
               }
               ?></div>
-             <div class="d-flex justify-content-center"><button class="btn_custum btn btn-center text-capitalize text-center justify-content-center">
+             <div class="d-flex justify-content-center"><button class="btn btn-primary btn-round btn-center text-capitalize text-center justify-content-center">
                 All topics
     </button></div> 
               <?php
