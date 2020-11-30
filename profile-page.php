1ea1e3a47695fc71c1fb765c5c62df0e7466ad83
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2020-11-30 19:33:00 +0100

    update disign button photo submit4

--- profile-page.php
+++ profile-page.php
@@ -179,7 +179,7 @@
                       scr=""
                        alt="Gravatar"
                      /><br>
-                     <div class="form-inline">
+                     <div class="justify-content-center">
                      <form action="#upload" method='post' enctype="multipart/form-data">
                       <input type="file" class="btn btn-round btn-outline-primary btn-sm" name="file"/><br><br>
                     	<input type="submit" class="btn btn-round btn-outline-primary btn-sm" value="Upload"/>
