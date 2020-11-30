5a5fd9338586a8f713007c4e833dbbcbb8dd2286
AliMundher <mundhersaeedali@gmail.com>
2020-11-30 15:32:17 +0100

    add test

--- board.php
+++ board.php
@@ -17,7 +17,7 @@ session_start();
 ob_start();
 require ('connect.php');
 $redirect = false;
-$boardId = is_numeric($_GET["board_id"]) ? intval($_GET["board_id"]): 0;
+$boardId = isset($_GET["board_id"]) && is_numeric($_GET["board_id"]) ? intval($_GET["board_id"]): 0;
 $board_result = NULL;
 
 if (!isset($_GET["board_id"]))
