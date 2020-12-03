16cef7befcec281fd8fbc0af26fd6ecb6b592750
Cédric debroux <71833601+Cedricdebroux@users.noreply.github.com>
2020-12-03 11:13:50 +0100

    new topic 6 WIP

--- new-topic.php
+++ new-topic.php
@@ -15,7 +15,7 @@ $terror ='';
          $contenu = htmlspecialchars($_POST['content']);
          if(!empty($sujet) AND !empty($contenu)) {
             if(strlen($sujet) <= 70) {
-               $ins = $conn->prepare('INSERT INTO `topics` (`title`, `creation_date`, `content`, `board_id`, `topic_by`) VALUES(?,NOW(),?,?,?');
+               $ins = $conn->prepare('INSERT INTO `topics` (`title`, `content`, `board_id`, `topic_by`, `creation_date`) VALUES(?,?,?,?,NOW()');
                $ins->execute(array($sujet,$contenu,$_GET['board_id'],$_SESSION['id']));
             } else {
                $terror = "Votre sujet ne peut pas dépasser 70 caractères";
