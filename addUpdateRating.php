716b28111bb9cecc146cb22cc11cda4009b2ce7c
User <rachida.haddouch.webdev@gmail.com>
2020-12-07 16:19:47 +0100

    reaction

--- addUpdateRating.php
+++ addUpdateRating.php
@@ -19,17 +19,18 @@ $member_id = $_SESSION['id'];
 if (! empty($_POST["rating"]) && ! empty($_POST["id"])) {
     require_once ("Rate.php");
     $rate = new Rate();
-    var_dump($_POST);
+    
     
     $ratingResult = $rate->getRatingByMessageForMember($_POST["id"], $member_id);
+    var_dump($ratingResult);
     if (! empty($ratingResult)) {
         $rate->updateRating($_POST["rating"], $ratingResult[0]["id"]);
     } else {
         $rate->addRating($_POST["id"], $_POST["rating"], $member_id);
-    }
+    }var_dump($ratingResult);
     
     $postRating = $rate->getRatingByMessage($_POST["id"]);
-    
+    var_dump($postRating);
     if (! empty($postRating[0]["rating_count"])) {
         echo $postRating[0]["rating_count"] . " Likes";
         if (! empty($postRating[0]["emoji_rating"])) {
