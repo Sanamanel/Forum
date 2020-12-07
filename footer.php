43b967912aea46869e1980849665ddadabd12da2
User <rachida.haddouch.webdev@gmail.com>
2020-12-07 15:52:53 +0100

    reaction

--- footer.php
+++ footer.php
@@ -45,9 +45,9 @@
 
 
     <script
-      src="./assets/js/core/jquery.min.js"
-      type="text/javascript"
-    ></script>
+  src="https://code.jquery.com/jquery-3.5.1.min.js"
+  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+  crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/emojionearea/3.4.2/emojionearea.min.js"></script>
 
     <script
