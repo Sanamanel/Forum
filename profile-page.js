0cefa9abfe17c0fac09e105d1ced78aa4f517b61
JonathanMaillard <jonathan.maillard95@gmail.com>
2020-12-09 16:07:45 +0100

    test blob

--- profile-page.js
+++ profile-page.js
@@ -25,9 +25,7 @@
         
         //If avatar is set, select it or show gravatar
         if (profile.image != null) {
-          alert("hello");
-          document.getElementById("avatar").src = "test"; 
-          /*"data:image/".(profile.type).";base64," . base64_encode(profile.image);*/
+          document.getElementById("avatar").src = "data:image/".(profile.type).";base64,".(profile.image);
         } else {
           document.getElementById("avatar").src = "https://2.gravatar.com/avatar/" + md5(profile.email.toLowerCase()) + "?s=150";
         }
