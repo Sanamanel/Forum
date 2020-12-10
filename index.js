304c3a07b1c01a9337f3ace19623aab757456eba
AliMundher <mundhersaeedali@gmail.com>
2020-12-10 13:42:57 +0100

    add event enter when user click the button enter

--- index.js
+++ index.js
@@ -2,6 +2,18 @@
   let baseUrl = "https://led-zepplin-forum.herokuapp.com/";
 
   document.getElementById("btn_login").addEventListener("click", async () => {
+    logIn();
+   
+  });
+
+  $('.input').on('keyup',function(e){
+      if(e.keyCode == 13){
+        logIn();
+      }
+  });
+
+  function logIn(){
+
     document.getElementById("login_errors").innerHTML = "";
     let emailValue = document.querySelector("#login_email").value;
     if (!emailValue) {
@@ -38,65 +50,78 @@
         }
       },
     });
-  });
 
-  document
-    .getElementById("btn_register")
-    .addEventListener("click", async () => {
-      document.getElementById("register_errors").innerHTML = "";
-      let usernameValue = document.querySelector("#register_username").value;
-      if (!usernameValue) {
-        document.getElementById("register_errors").innerHTML =
-          "Please enter an username";
-        return;
-      }
 
-      let passwordValue = document.querySelector("#register_password").value;
-      if (!passwordValue) {
-        document.getElementById("register_errors").innerHTML =
-          "Please enter a password";
-        return;
-      }
+}
 
-      let passwordConfirmationValue = document.querySelector(
-        "#register_password_confirmation"
-      ).value;
-      if (!passwordConfirmationValue) {
-        document.getElementById("register_errors").innerHTML =
-          "Please enter a confirmation password";
-        return;
-      }
+document.getElementById("btn_register").addEventListener("click", async () => {
+    register();
 
-      let emailValue = document.querySelector("#register_email").value;
-      if (!emailValue) {
-        document.getElementById("register_errors").innerHTML =
-          "Please enter an email address ";
-        return;
-      }
+});
 
-      let content = {
-        action: "register",
-        username: usernameValue,
-        password: passwordValue,
-        passwordConfirmation: passwordConfirmationValue,
-        email: emailValue,
-      };
-      let contentStr = JSON.stringify(content);
-      $.ajax({
-        //send a post request to the server with the content as JSON
-        url: baseUrl + "config/server.php",
-        type: "post",
-        data: { content: contentStr },
-        complete: function (response) {
-          console.log(response);
-          if (response.status == 200)
-            window.location.href = baseUrl + "home.php";
-          //An user that is registered, is automatically logged
-          else {
-            document.getElementById("register_errors").innerHTML =
-              response.responseText;
-          }
-        },
-      });
+function register(){
+
+    document.getElementById("register_errors").innerHTML = "";
+    let usernameValue = document.querySelector("#register_username").value;
+    if (!usernameValue) {
+      document.getElementById("register_errors").innerHTML =
+        "Please enter an username";
+      return;
+    }
+
+    let passwordValue = document.querySelector("#register_password").value;
+    if (!passwordValue) {
+      document.getElementById("register_errors").innerHTML =
+        "Please enter a password";
+      return;
+    }
+
+    let passwordConfirmationValue = document.querySelector(
+      "#register_password_confirmation"
+    ).value;
+    if (!passwordConfirmationValue) {
+      document.getElementById("register_errors").innerHTML =
+        "Please enter a confirmation password";
+      return;
+    }
+
+    let emailValue = document.querySelector("#register_email").value;
+    if (!emailValue) {
+      document.getElementById("register_errors").innerHTML =
+        "Please enter an email address ";
+      return;
+    }
+
+    let content = {
+      action: "register",
+      username: usernameValue,
+      password: passwordValue,
+      passwordConfirmation: passwordConfirmationValue,
+      email: emailValue,
+    };
+    let contentStr = JSON.stringify(content);
+    $.ajax({
+      //send a post request to the server with the content as JSON
+      url: baseUrl + "config/server.php",
+      type: "post",
+      data: { content: contentStr },
+      complete: function (response) {
+        console.log(response);
+        if (response.status == 200)
+          window.location.href = baseUrl + "home.php";
+        //An user that is registered, is automatically logged
+        else {
+          document.getElementById("register_errors").innerHTML =
+            response.responseText;
+        }
+      },
     });
-})();
+  
+
+
+
+}
+
+
+
+  })();
