64e51d2f236bf7d0a6999ffbfaab94f292b2dd84
User <rachida.haddouch.webdev@gmail.com>
2020-12-11 15:44:31 +0100

    test-search

--- search_result.php
+++ search_result.php
@@ -1,15 +1,7 @@
 <?php
 	define("SITE_ADDR","https://led-zepplin-forum.herokuapp.com/");
 	include("config/server.php");
-	session_start(); 
-// If the session variable is empty, this  
-        // means the user is yet to login 
-        // User will be sent to 'login.php' page 
-        // to allow the user to login 
-        if(!isset($_SESSION['username'])){ 
-            header('location: https://led-zepplin-forum.herokuapp.com/'); 
-            exit();
-        }
+
 
 	include ("header.php");
 ?>
@@ -21,7 +13,7 @@
   <div class="section text-center">
 				
 				<center>
-				<form action="search_result.php?search=" method="GET" name="">
+				<form action="test-search.php?search=" method="GET" name="">
 					<table>
 						<tr>
 							<td> <input
